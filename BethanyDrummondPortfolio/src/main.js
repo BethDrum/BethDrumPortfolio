90051d3f070d8aac41e9f6760a8bc9c7e4134e78
BethDrum <bethany.drummond@gmail.com>
2026-08-18 14:34:25 +0100

    temp fix while trying other methods - This will work for a vertical scrolling section BUT not how intended origionally

--- BethanyDrummondPortfolio/src/main.js
+++ BethanyDrummondPortfolio/src/main.js
@@ -3,11 +3,38 @@ import './style.css'
 gsap.registerPlugin(ScrollTrigger);
 
 const contents = gsap.utils.toArray('#horizontal .section')
-gsap.to(contents, {
+const horizontalSec = gsap.to(contents, {
   xPercent: -100 * (contents.length -1),
+  ease: "none",
   scrollTrigger: {
-    trigger: "#horizontal",
+    trigger: "#scrollWrap",
+    pin: true,
+    scrub: 1,
+    start: "top top",
+    end: "+=" + window.innerWidth * contents.length
+  }
+});
+
+const vertical = gsap.utils.toArray('#vertical .sectionV')
+gsap.to(vertical, {
+  yPercent: -100 * (vetical.length -1),
+  ease: "none",
+  scrollTrigger:{
+    trigger:"#verticalSec",
+    containerAnimation: horizontalSec,
+    start: "left right",
+    end: "+=" + window.innerHeight * vertical.length,
     pin: true,
     scrub: 1,
   }
-})
+});
+
+// const horizontalEnd = gsap.utils.toArray('#horizontalEnd .section')
+// gsap.to(horizontalEnd, {
+//   xPercent: -100 * (horizontalEnd.length -1),
+//   scrollTrigger: {
+//     trigger: "#horizontalEnd",
+//     pin: true,
+//     scrub: 1,
+//   }
+// })
