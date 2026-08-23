f02e68ccddca4e4d493f5e5967f14e9b4d4939c8
BethDrum <bethany.drummond@gmail.com>
2026-08-23 18:51:55 +0100

    vertical scrolling fix

--- BethanyDrummondPortfolio/src/main.js
+++ BethanyDrummondPortfolio/src/main.js
@@ -2,39 +2,61 @@ import './style.css'
 
 gsap.registerPlugin(ScrollTrigger);
 
-const contents = gsap.utils.toArray('#horizontal .section')
-const horizontalSec = gsap.to(contents, {
-  xPercent: -100 * (contents.length -1),
-  ease: "none",
+const contents = gsap.utils.toArray('#horizontal .section');
+const contentsV = gsap.utils.toArray('#vertical .sectionV');
+const contentsF = gsap.utils.toArray('#horizontalF .sectionF');
+
+const tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#scrollWrap",
     pin: true,
     scrub: 1,
     start: "top top",
-    end: "+=" + window.innerWidth * contents.length
+    end: "+=" + window.innerWidth * (
+      contents.length +
+      contentsV.length +
+      contentsF.length
+    )
   }
 });
 
-const vertical = gsap.utils.toArray('#vertical .sectionV')
-gsap.to(vertical, {
-  yPercent: -100 * (vetical.length -1),
-  ease: "none",
-  scrollTrigger:{
-    trigger:"#verticalSec",
-    containerAnimation: horizontalSec,
-    start: "left right",
-    end: "+=" + window.innerHeight * vertical.length,
-    pin: true,
-    scrub: 1,
-  }
+tl.to("#horizontal", {
+  opacity: 1,
+  duration: 0,
+}, "<");
+
+tl.to(contents, {
+  xPercent: -100 * (contents.length - 1),
+  ease: "none"
 });
 
-// const horizontalEnd = gsap.utils.toArray('#horizontalEnd .section')
-// gsap.to(horizontalEnd, {
-//   xPercent: -100 * (horizontalEnd.length -1),
-//   scrollTrigger: {
-//     trigger: "#horizontalEnd",
-//     pin: true,
-//     scrub: 1,
-//   }
-// })
+tl.to("#horizontal",{
+  opacity: 0,
+  duration: 0.3,
+});
+
+//vertical portfolio
+tl.to("#vertical", {
+  opacity: 1,
+  duration: 0,
+}, "<");
+
+tl.to(contentsV, {
+  yPercent: -100 * (contentsV.length - 1),
+  ease: "none"
+});
+
+tl.to("#vertical",{
+  opacity: 0,
+  duration: 0.1
+});
+
+tl.to("#horizontalF",{
+  opacity: 1,
+  duration: 0
+});
+
+tl.to(contentsF, {
+  xPercent: -100 * (contentsF.length - 1),
+  ease: "none"
+});
