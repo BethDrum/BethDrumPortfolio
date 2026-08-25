17542e4eaad25ff6245e1e8d1055dadda13f93c1
BethDrum <bethany.drummond@gmail.com>
2026-08-25 15:41:13 +0100

    smoother animation

--- BethanyDrummondPortfolio/src/main.js
+++ BethanyDrummondPortfolio/src/main.js
@@ -20,42 +20,48 @@ const tl = gsap.timeline({
   }
 });
 
-tl.to("#horizontal", {
-  opacity: 1,
-  duration: 0,
-}, "<");
+// Horizontal fade-in
+tl.to("#horizontal", { opacity: 1, duration: 0 });
 
+// Horizontal scroll
 tl.to(contents, {
   xPercent: -100 * (contents.length - 1),
   ease: "none"
 });
 
-tl.to("#horizontal",{
-  opacity: 0,
-  duration: 0,
-});
+// Fade horizontal out slightly (not fully)
+tl.to("#horizontal", { opacity: 0.3, duration: 0.4 });
 
-//vertical portfolio
+// Vertical fade-in + float effect
 tl.to("#vertical", {
   opacity: 1,
-  duration: 0,
+  scale: 1,
+  duration: 0.8,
+  ease: "power2.out"
 }, "<");
 
+// Vertical scroll
 tl.to(contentsV, {
   yPercent: -100 * (contentsV.length - 1),
   ease: "none"
 });
 
-tl.to("#vertical",{
+// Vertical fade-out
+tl.to("#vertical", {
   opacity: 0,
-  duration: 0
+  scale: 0.95,
+  duration: 0.6,
+  ease: "power2.in"
 });
 
-tl.to("#horizontalF",{
+// Fade next horizontal section in
+tl.to("#horizontalF", {
   opacity: 1,
-  duration: 0
+  duration: 0,
+  ease: "power2.out"
 });
 
+// Final horizontal scroll
 tl.to(contentsF, {
   xPercent: -100 * (contentsF.length - 1),
   ease: "none"
