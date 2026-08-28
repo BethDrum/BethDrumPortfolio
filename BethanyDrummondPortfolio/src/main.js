ef792b7c838b3745dee140fba9638d59f8265453
BethDrum <bethany.drummond@gmail.com>
2026-08-28 23:14:31 +0100

    all links added

--- BethanyDrummondPortfolio/src/main.js
+++ BethanyDrummondPortfolio/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const contents = gsap.utils.toArray('#horizontal .section');
 const contentsV = gsap.utils.toArray('#vertical .sectionV');
@@ -74,38 +74,3 @@ tl.to(contentsF, {
   xPercent: -100 * (contentsF.length - 1),
   ease: "none"
 });
-
-
-//add variables for tracking buttons/links
-
-//CV button first
-const target = document.getElementById("visableCVButton");
-const clickB = document.getElementById("cvButton");
-
-function trackButton() {
-  //failsafe for if either not present
-  if (!target || !clickB ) return;
-
-  const rect = target.getBoundingClientRect();
-  
-  //set position of current invisable button
-  gsap.set(clickB, {
-    x: rect.left,
-    y: rect.top,
-    width: rect.width || 200,
-    height: rect.height || 200
-  });
-}
-
-//trigger trackButton() at any/all parts of the scroll
-ScrollTrigger.addEventListener("refresh", trackButton);
-ScrollTrigger.addEventListener("scrollStart", trackButton);
-ScrollTrigger.addEventListener("scrollEnd", trackButton);
-gsap.ticker.add(trackButton)
-
-//add fallbacks for rapid resizing or layout change
-window.addEventListener("resize", trackButton);
-window.addEventListener("DOMContentLoaded", trackButton);
-
-//initial call to function
-trackButton();
